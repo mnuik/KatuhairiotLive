eaf1b111f3f9688277454ea97ae2d5cae90f0574
Mira Muikku <mira.muikku@awacademy.training>
2022-08-25 11:32:21 +0300

    Muutettu config: optionsBuilder

--- KatuhairiotLive/Controllers/HomeController.cs
+++ KatuhairiotLive/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using KatuhairiotLive.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace KatuhairiotLive.Controllers
@@ -7,10 +8,17 @@ namespace KatuhairiotLive.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public readonly HkarttaDBContext db;
+        public readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+
+            var optionsBuilder = new DbContextOptionsBuilder<HkarttaDBContext>();
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("HkarttaDB"));
+            db = new HkarttaDBContext(optionsBuilder.Options);
         }
 
         public IActionResult Index()
---
+++ KatuhairiotLive/Models/Username.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KatuhairiotLive.Models
+{
+    public partial class Username
+    {
+        public int UserId { get; set; }
+        public string Username1 { get; set; } = null!;
+        public int? Routeid { get; set; }
+        public string? FavRoute { get; set; }
+
+        public virtual URoute? Route { get; set; }
+    }
+}
