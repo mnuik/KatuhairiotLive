5f54d670364c0abfc1031b1777f997e413b59dc1
Mira Muikku <mira.muikku@awacademy.training>
2022-08-26 10:59:49 +0300

    modattu home-index-viewhun table ja valikot, lisätty k-osa-malli kirjastoon

---
+++ KatutyotLib/Models/Kaupunginosa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatutyotLib.Models
+{
+    public class Kaupunginosa
+    {
+        public int id; // Kohteen yksilöivä tunniste.Luotettava, kattavasti olemassa oleva tieto.
+        public string aluejako; // Aluejaon tyyppi.Luotettava, kattavasti olemassa oleva tieto.
+        public string kunta; // Kunta.Luotettava, kattavasti olemassa oleva tieto.
+        public string tunnus; // Kaupunginosan tunnus. Luotettava, kattavasti olemassa oleva tieto.
+        public string nimi_fi; // Kaupunginosan suomenkielinen nimi.Luotettava, kattavasti olemassa oleva tieto.
+        public string nimi_se; // Kaupunginosan ruotsinkielinen nimi.Luotettava, kattavasti olemassa oleva tieto.
+        public DateTime yhtluontipvm; // Kohteen luontipäivämäärä (yyyy-mm-dd). Luotettava, kattavasti olemassa oleva tieto.
+        public DateTime yhtmuokkauspvm; // Kohteen edellinen muokkauspäivämäärä(yyyy-mm-dd). Luotettava, kattavasti olemassa oleva tieto.
+        public string yhtdatanomistaja; // Aineiston omistaja.Luotettava, kattavasti olemassa oleva tieto.
+        public DateTime paivitetty_tietopalveluun; // Tietopalvelun tallennuspäivämäärä (yyyy-mm-dd). Luotettava, kattavasti olemassa oleva tieto.
+    }
+}
