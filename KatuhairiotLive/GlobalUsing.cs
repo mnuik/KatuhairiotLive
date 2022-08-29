c5720b68b26c560dc96bfdd7353684c14ea3f2ae
Andrevaan <koippiiska@gmail.com>
2022-08-29 13:53:43 +0300

    Kirjautuminen onnistuu nyt minun localhostilla, pitää laittaa siihen tuo meidän azuren osoite! Pitää vielä laittaa palikat oikein

---
+++ KatuhairiotLive/GlobalUsing.cs
@@ -0,0 +1,14 @@
+﻿global using Auth0.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authentication;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace KatuhairiotLive
+    
+{
+    public class GlobalUsing
+    {
+    }
+}
---
+++ KatuhairiotLive/Models/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace KatuhairiotLive.Models
+{
+    public class UserProfile
+    {
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
