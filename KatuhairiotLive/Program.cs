7c44576ef2b8c220d11a5a1dc99bb659c7e4dfba
mira.muikku <mira.muikku@avanade.com>
2022-09-27 15:47:03 +0300

    Tyyt

--- KatuhairiotLive/Program.cs
+++ KatuhairiotLive/Program.cs
@@ -31,4 +31,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+app.MapControllerRoute(
+    name: "kaupunginosat",
+    pattern: "{controller=Kaupunginosa}/{action=Index}/{id?}");
+
+
 app.Run();
